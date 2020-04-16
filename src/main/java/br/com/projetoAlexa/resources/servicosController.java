59f00a38308737184db58f2a399011de411c0130
Eduardo Rodrigues Fernandes <eduardo.fernandes@spread.com.br>
2020-04-16 18:50:59 -0300

    endpoint padrão

---
+++ src/main/java/br/com/projetoAlexa/resources/servicosController.java
@@ -0,0 +1,38 @@
+package br.com.projetoAlexa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class servicosController {
+	
+	@GetMapping()
+	public ResponseEntity<?> get()  {
+		return ResponseEntity.badRequest().body("Sistema em construção, servidor no ar!");
+	}
+	
+	@PostMapping()
+	public ResponseEntity<?> post()  {
+		return ResponseEntity.badRequest().body("Sistema em construção, servidor no ar!");
+	}
+	
+	
+	@PutMapping()
+	public ResponseEntity<?> put()  {
+		return ResponseEntity.badRequest().body("Sistema em construção, servidor no ar!");
+	}
+	
+	
+	@DeleteMapping()
+	public ResponseEntity<?> delete()  {
+		return ResponseEntity.badRequest().body("Sistema em construção, servidor no ar!");
+	}
+	
+
+}
